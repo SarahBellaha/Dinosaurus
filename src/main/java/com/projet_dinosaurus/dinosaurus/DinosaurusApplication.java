a5679c73165f093933711e60a666d154954dc80a
BELLAHA Sarah (SNCF VOYAGEURS / Etablissement Gares Transilien Sud Ligne C / TERR JUINE ET ORGE) <9008080s@commun.ad.sncf.fr>
2022-11-21 15:48:00 +0100

    initial commit

---
+++ src/main/java/com/projet_dinosaurus/dinosaurus/DinosaurusApplication.java
@@ -0,0 +1,15 @@
+package com.projet_dinosaurus.dinosaurus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinosaurusApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DinosaurusApplication.class, args);
+		System.out.println("HELLO WORLD !");
+	}
+
+}
